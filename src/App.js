f5b10312dcabda209b1ef6dfc4f3564ebf774acf
KadenHerr <kherr@email.arizona.edu>
2021-05-05 13:21:40 -0700

    Removed unneeded code to clear warnings

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import Container from 'react-bootstrap/Container';
 import NavigationBar from "./components/NavigationBar";
-import { useState } from 'react';
-import Place from './components/Place';
 import Home from './components/Home';
 
 function App() {
